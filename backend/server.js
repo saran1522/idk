4ead20cfdf0b99e3bcb7f93c25606185e2dc8fac
sundaram <sundaram.2911@gmail.com>
2024-08-08 00:07:10 +0530

    Issue with server resolved!

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Create the Express app
+const app = express();
+
+// Middleware
+app.use(cors()); // Enable CORS if needed
+app.use(bodyParser.json()); // Parse JSON bodies
+
+// Route Definitions
+app.get('/', (req, res) => {
+    res.send("hello , its working!!!");
+});
+app.listen(3000,(req,res)=>{
+    console.log("Server listening on 3000..");
+})
